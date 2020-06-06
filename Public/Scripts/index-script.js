9c22da27456849be1f821863f6813c123a5d4020
Ntladi <ntladimohajane@gmail.com>
2020-06-06 19:44:01 +0200

    Added CSS styling for sign up page

--- Public/Scripts/index-script.js
+++ Public/Scripts/index-script.js
@@ -9,6 +9,7 @@ const newInputField = function (id, placeholder, labelText, required) {
   field.setAttribute('id', id)
   field.setAttribute('name', id)
   field.required = required
+  field.setAttribute('type', 'text')
 
   // set label attributes
   label.textContent = labelText
@@ -21,7 +22,11 @@ const signUpForm = function () {
   // initialise DOM attributes for the signup form
   const form = document.createElement('form')
   const container = document.createElement('div')
+  const heading = document.createElement('h1')
+  const line = document.createElement('hr')
+  const buttons = document.createElement('div')
   const submitButton = document.createElement('button')
+  const cancelButton = document.createElement('button')
 
   // initialise all fields
   const nameField = newInputField('sign-up-name', 'Enter Name', 'Name', true)
@@ -38,13 +43,26 @@ const signUpForm = function () {
 
   // set container attributes
   container.classList = 'container'
+  buttons.classList = 'modal-buttons'
+
+  // set heading attributes
+  heading.textContent = 'Sign Up'
 
   // set button attributes
-  submitButton.textContent = 'Create Account'
+  submitButton.textContent = 'Create'
+  cancelButton.textContent = 'Cancel'
   submitButton.setAttribute('type', 'submit')
-  submitButton.classList = 'submitButton'
+  cancelButton.setAttribute('type', 'button')
+  submitButton.classList = 'submit-button'
+  cancelButton.classList = 'cancel-button'
+
+  // append buttons to their container
+  buttons.appendChild(submitButton)
+  buttons.appendChild(cancelButton)
 
   // append DOM attributes to container
+  container.appendChild(heading)
+  container.appendChild(line)
   container.appendChild(nameField[1])
   container.appendChild(nameField[0])
   container.appendChild(surnameField[1])
@@ -55,11 +73,24 @@ const signUpForm = function () {
   container.appendChild(emailField[0])
   container.appendChild(passField[1])
   container.appendChild(passField[0])
-  container.appendChild(submitButton)
+  container.appendChild(buttons)
 
   // append container to form
   form.appendChild(container)
 
   return form
 }
-document.querySelector('#sign-up-box').appendChild(signUpForm())
+
+const signUpBox = document.querySelector('#sign-up-box')
+const signUpButton = document.querySelector('#sign-up-button')
+signUpBox.appendChild(signUpForm())
+const cancelButton = document.querySelector('.cancel-button')
+
+window.onclick = function (event) {
+  if (event.target === signUpBox || event.target === cancelButton) {
+    signUpBox.style.display = 'none'
+  }
+  if (event.target === signUpButton) {
+    signUpBox.style.display = 'block'
+  }
+}
