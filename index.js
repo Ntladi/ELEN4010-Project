2ac1ce325dff5d558d82635b500cb0a6af8d6957
Gerald Kirui <geraldkirui8@gmail.com>
2020-06-15 21:27:33 +0200

    Secure Login Implemented

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const express = require('express') // load the express module
 const app = express()
 const bodyParser = require('body-parser') // load the body parser
 const mainRouter = require('./Backend/mainRoutes.js') // load the main router
+const multer = require('multer') // load multipart form processor
+const upload = multer()
+const session = require('express-session') // load session middle-ware processor
+const cookieParser = require('cookie-parser') // load cookie-parser for session
 
-// tell express to use ejs as the view engine
+// tell Express to use Express as the view engine
 app.set('view engine', 'ejs')
 app.set('views', './Views')
 
@@ -14,6 +18,15 @@ app.set('views', './Views')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/cdn', express.static('Public'))
+
+// tell Express to use the loaded middleware
+app.use(upload.array())
+app.use(cookieParser())
+app.use(session({
+  secret: 'cookie_secret',
+  resave: true,
+  saveUninitialized: true
+}))
 
 // mount the router
 app.use('/', mainRouter)
