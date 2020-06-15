1b83f5e237870116c5f1fbd4efa32bb8161b875d
Learn <leago89@gmail.com>
2020-06-15 08:40:44 +0200

    Added EJS Dependencies for View Engine

--- Backend/mainRoutes.js
+++ Backend/mainRoutes.js
@@ -1,16 +1,15 @@
 'use strict'
 
-const path = require('path')
 const dataManager = require('./dbManagement')
 const express = require('express')
 const mainRouter = express.Router()
 
 mainRouter.get('/', function (req, res) { // the welcome page
-  res.sendFile(path.join(__dirname, '..', 'Views', 'index.html'))
+  res.render('index.ejs')
 })
 
 mainRouter.get('/home', function (req, res) { // the main home page once signed in
-  res.sendFile(path.join(__dirname, '..', 'Views', 'home.html'))
+  res.render('home.ejs')
 })
 
 mainRouter.post('/create-user', function (req, res) { // will handle the user creation process
