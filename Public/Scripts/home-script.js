7592b52d01faf285927f24bd113076b62927d67c
PendapalaS <pendapala.shipiki@gmail.com>
2020-06-23 19:15:31 +0200

    Added box for filtering expenses

--- Public/Scripts/home-script.js
+++ Public/Scripts/home-script.js
@@ -74,12 +74,79 @@ const newExpenseForm = function () {
   return form
 }
 
+const newFilterForm = function () {
+  // initialise DOM attributes for the signup form
+  const form = document.createElement('form')
+  const container = document.createElement('div')
+  const heading = document.createElement('h1')
+  const line = document.createElement('hr')
+  const buttons = document.createElement('div')
+  const submitButton = document.createElement('button')
+  const myExpensesButton = document.createElement('button')
+  const pendingDebtsButton = document.createElement('button')
+  const settledDebtsButton = document.createElement('button')
+  const otherExpensesButton = document.createElement('button')
+
+  // initialise all fields
+
+  // set heading attributes
+  heading.textContent = 'Filter Expenses'
+  // set form attributes
+  form.classList = 'modal-box'
+  // set container attributes
+  container.classList = 'submission-form'
+
+  // set button attributes
+  submitButton.textContent = 'Apply'
+  submitButton.classList = 'modal-submit-button'
+  submitButton.setAttribute('type', 'button')
+
+  // set button attributes
+  myExpensesButton.textContent = 'Show My Posted Expenses'
+  myExpensesButton.classList = 'modal-filter-button'
+  myExpensesButton.setAttribute('type', 'button')
+  myExpensesButton.setAttribute('id', 'show-my-expenses-button')
+
+  pendingDebtsButton.textContent = 'Show My Pending Expenses'
+  pendingDebtsButton.classList = 'modal-filter-button'
+  pendingDebtsButton.setAttribute('type', 'button')
+  pendingDebtsButton.setAttribute('id', 'show-my-pending-debts-button')
+
+  settledDebtsButton.textContent = 'Show My Settled Expenses'
+  settledDebtsButton.classList = 'modal-filter-button'
+  settledDebtsButton.setAttribute('type', 'button')
+  settledDebtsButton.setAttribute('id', 'show-my-settled-debts-button')
+
+  otherExpensesButton.textContent = 'Show Other Expenses'
+  otherExpensesButton.classList = 'modal-filter-button'
+  otherExpensesButton.setAttribute('type', 'button')
+  otherExpensesButton.setAttribute('id', 'show-my-settled-debts-button')
+
+  // append buttons to their container
+  buttons.appendChild(submitButton)
+
+  // append DOM attributes to container
+  container.appendChild(heading)
+  container.appendChild(line)
+  container.appendChild(myExpensesButton)
+  container.appendChild(pendingDebtsButton)
+  container.appendChild(settledDebtsButton)
+  container.appendChild(otherExpensesButton)
+  container.appendChild(document.createElement('br'))
+  container.appendChild(buttons)
+  form.appendChild(container)
+
+  return form
+}
+
 const addExpenseBox = document.querySelector('#add-expense-box')
 addExpenseBox.appendChild(newExpenseForm())
 
+const filterExpenseBox = document.querySelector('#filter-expense-box')
+filterExpenseBox.appendChild(newFilterForm())
+
 const addExpenseButton = document.querySelector('#add-expense-button')
-const personalLogButton = document.querySelector('#personal-log-box')
-const householdLogButton = document.querySelector('#household-log-box')
+const filterCardsButton = document.querySelector('#filter-expenses-button')
 const addExpenseCancelButton = document.querySelector('#add-expense-cancel-button')
 const allExpenses = document.querySelector('#expense-card-container')
 listExpenses(allExpenses)
@@ -90,5 +157,11 @@ window.onclick = function (event) {
   }
   if (event.target === addExpenseButton) {
     addExpenseBox.style.display = 'block'
+  }
+  if (event.target === filterCardsButton) {
+    filterExpenseBox.style.display = 'block'
+  }
+  if (event.target === filterExpenseBox) {
+    filterExpenseBox.style.display = 'none'
   }
 }
